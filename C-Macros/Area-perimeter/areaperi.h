d93efd67cbed673b052be6b0a47fe4d69350c878
ishitakarandikar <44649228+ishitakarandikar@users.noreply.github.com>
2021-06-21 14:21:55 +0530

    Create areaperi.h

---
+++ C-Macros/Area-perimeter/areaperi.h
@@ -0,0 +1,6 @@
+#define TP(a,b,c) (a+b+c)                                /* perimeter of triangle */
+#define TA(a,b,c,d) (d*((d-a)*(d-b)*(d-c)))      /* area of triangle *
+#define PS(x) (4*x)                                          /* perimeter of square */
+#define SA(x) (x*x)                                           /* area of square */
+#define CP(x) (2*3.14*r)                                 /* perimeter of circle */
+#define CA(x) (3.14*r*r)                                  /* area of circle */
